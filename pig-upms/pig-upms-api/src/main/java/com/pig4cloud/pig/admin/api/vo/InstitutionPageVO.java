d8725cadee1ba19d49687a3a911f9ac0aa9232f8
缪建华 <2011348742@qq.com>
2022-01-28 15:59:23 +0800

    修改机构mapper问题

--- pig-upms/pig-upms-api/src/main/java/com/pig4cloud/pig/admin/api/vo/InstitutionPageVO.java
+++ pig-upms/pig-upms-api/src/main/java/com/pig4cloud/pig/admin/api/vo/InstitutionPageVO.java
@@ -17,83 +17,71 @@
 
 package com.pig4cloud.pig.admin.api.vo;
 
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.annotation.TableLogic;
-import com.baomidou.mybatisplus.annotation.TableName;
-import com.pig4cloud.pig.admin.api.entity.Address;
-import com.pig4cloud.pig.common.mybatis.base.BaseEntity;
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
 
 /**
  * 机构分页VO
  */
 @Data
-public class InstitutionPageVO {
+public class InstitutionPageVO  implements Serializable {
 
 	/**
 	 * 机构id 主键自增
 	 */
-	@TableId
-	@ApiModelProperty(value = "机构id 主键自增")
 	private Integer insId;
+
 	/**
 	 * 机构名称
 	 */
-	@ApiModelProperty(value = "机构名称")
 	private String insName;
+
 	/**
 	 * 机构类型
 	 */
-	@ApiModelProperty(value = "机构类型")
 	private Integer insType;
+
 	/**
 	 * 是否禁用 0-否 1-是
 	 */
-	@ApiModelProperty(value = "是否禁用 0-否 1-是")
 	private Integer canDisable;
+
 	/**
 	 * 传真
 	 */
-	@ApiModelProperty(value = "传真")
 	private String insFax;
+
 	/**
 	 * 座机电话
 	 */
-	@ApiModelProperty(value = "座机电话")
 	private String insLandLinePhone;
+
 	/**
 	 * 备注
 	 */
-	@ApiModelProperty(value = "备注")
 	private String insRemark;
+
 	/**
 	 * 删除状态(0-正常,1-已删除)
 	 */
-	@TableLogic
 	private String delFlag;
 
 	/**
 	 * 省
 	 */
-	@ApiModelProperty(value = "省")
 	private String province;
 	/**
 	 * 市
 	 */
-	@ApiModelProperty(value = "市")
 	private String city;
 	/**
 	 * 区
 	 */
-	@ApiModelProperty(value = "区")
 	private String area;
 	/**
 	 * 信息地址
 	 */
-	@ApiModelProperty(value = "信息地址")
 	private String informationAddress;
 
 
