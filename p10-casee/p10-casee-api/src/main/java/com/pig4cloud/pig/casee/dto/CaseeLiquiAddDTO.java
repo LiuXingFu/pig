8cf8e00943af77bfea16e2f3777e40670a2886f9
xiels <Aa123654789>
2022-02-19 09:45:40 +0800

    修改添加案件接口 开发项目详情办理页面接口

--- p10-casee/p10-casee-api/src/main/java/com/pig4cloud/pig/casee/dto/CaseeLiquiAddDTO.java
+++ p10-casee/p10-casee-api/src/main/java/com/pig4cloud/pig/casee/dto/CaseeLiquiAddDTO.java
@@ -16,90 +16,100 @@ public class CaseeLiquiAddDTO {
 	/**
 	 * 案号
 	 */
-	@ApiModelProperty(value="案号")
+	@ApiModelProperty(value = "案号")
 	private String caseeNumber;
 
 	/**
 	 * 案件类型(10000:诉前保全案件 10001:诉讼保全案件 10002:一审诉讼案件 10003:二审诉讼案件 10004:其它案件 10005:首次执行案件 10006:执灰案件)
 	 */
-	@ApiModelProperty(value="案件类型(10000:诉前保全案件 10001:诉讼保全案件 10002:一审诉讼案件 10003:二审诉讼案件 10004:其它案件 10005:首次执行案件 10006:执灰案件)")
+	@ApiModelProperty(value = "案件类型(10000:诉前保全案件 10001:诉讼保全案件 10002:一审诉讼案件 10003:二审诉讼案件 10004:其它案件 10005:首次执行案件 10006:执灰案件)")
 	private Integer caseeType;
 
 	/**
 	 * 立案日期
 	 */
-	@ApiModelProperty(value="立案日期")
+	@ApiModelProperty(value = "立案日期")
 	private LocalDate startTime;
 
 	/**
 	 * 司法费金额
 	 */
-	@ApiModelProperty(value="司法费金额")
+	@ApiModelProperty(value = "司法费金额")
 	private BigDecimal judicialExpenses;
 
 	/**
 	 * 父案件id
 	 */
-	@ApiModelProperty(value="父案件id")
+	@ApiModelProperty(value = "父案件id")
 	private Integer parentId;
 
 	/**
 	 * 类别（0-一审 1-二审 2-首执 3- 执恢）
 	 */
-	@ApiModelProperty(value="类别（0-一审 1-二审 2-首执 3- 执恢）")
+	@ApiModelProperty(value = "类别（0-一审 1-二审 2-首执 3- 执恢）")
 	private Integer category;
 
 	/**
 	 * 承办法院id
 	 */
-	@ApiModelProperty(value="承办法院id")
+	@ApiModelProperty(value = "承办法院id")
 	private Integer courtId;
 
 	/**
 	 * 法院部门id
 	 */
-	@ApiModelProperty(value="法院部门id")
+	@ApiModelProperty(value = "法院部门id")
 	private Integer judgeOutlesId;
 
 	/**
 	 * 承办法官id
 	 */
-	@ApiModelProperty(value="承办法官id")
+	@ApiModelProperty(value = "承办法官id")
 	private Integer judgeId;
 
 	/**
 	 * 法官名称
 	 */
-	@ApiModelProperty(value="法官名称")
+	@ApiModelProperty(value = "法官名称")
 	private String judgeName;
 
 	/**
 	 * 案件总金额
 	 */
-	@ApiModelProperty(value="案件总金额")
+	@ApiModelProperty(value = "案件总金额")
 	private BigDecimal caseeAmount;
 
-	@ApiModelProperty(value="清收案件详情表")
+	@ApiModelProperty(value = "清收案件详情表")
 	private CaseeLiquiDetail caseeLiquiDetail;
 
 	/**
 	 * 项目id
 	 */
-	@ApiModelProperty(value="项目id")
+	@ApiModelProperty(value = "项目id")
 	private Integer projectId;
 
 	/**
 	 * 办理人id
 	 */
-	@ApiModelProperty(value="办理人id")
+	@ApiModelProperty(value = "办理人id")
 	private Integer userId;
+
+	/**
+	 * 办理人名称
+	 */
+	@ApiModelProperty(value = "办理人名称")
+	private String actualName;
 
 	/**
 	 * 债务人集合
 	 */
-	@ApiModelProperty(value="债务人集合")
+	@ApiModelProperty(value = "债务人集合")
 	private List<Integer> subjectIdList;
 
+	/**
+	 * 律师名称
+	 */
+	String lawyerName;
 
 
 }
---
+++ p10-casee/p10-casee-biz/src/main/java/com/pig4cloud/pig/casee/service/impl/CaseeLawyerReServiceImpl.java
@@ -0,0 +1,34 @@
+/*
+ *    Copyright (c) 2018-2025, lengleng All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice,
+ * this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ * notice, this list of conditions and the following disclaimer in the
+ * documentation and/or other materials provided with the distribution.
+ * Neither the name of the pig4cloud.com developer nor the names of its
+ * contributors may be used to endorse or promote products derived from
+ * this software without specific prior written permission.
+ * Author: lengleng (wangiegie@gmail.com)
+ */
+package com.pig4cloud.pig.casee.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.pig4cloud.pig.casee.entity.CaseeLawyerRe;
+import com.pig4cloud.pig.casee.mapper.CaseeLawyerReMapper;
+import com.pig4cloud.pig.casee.service.CaseeLawyerReService;
+import org.springframework.stereotype.Service;
+
+/**
+ * 案件律师关联表
+ *
+ * @author pig code generator
+ * @date 2022-02-15 10:56:32
+ */
+@Service
+public class CaseeLawyerReServiceImpl extends ServiceImpl<CaseeLawyerReMapper, CaseeLawyerRe> implements CaseeLawyerReService {
+
+}
