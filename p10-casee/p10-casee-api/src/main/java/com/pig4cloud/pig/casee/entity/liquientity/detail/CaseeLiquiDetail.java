74c3e22b32f4b8749e772d00e9708147d0457cb9
xiels <Aa123654789>
2022-03-03 21:08:07 +0800

    解决查询案件详情报错问题

--- p10-casee/p10-casee-api/src/main/java/com/pig4cloud/pig/casee/entity/liquientity/detail/CaseeLiquiDetail.java
+++ p10-casee/p10-casee-api/src/main/java/com/pig4cloud/pig/casee/entity/liquientity/detail/CaseeLiquiDetail.java
@@ -2,7 +2,13 @@ package com.pig4cloud.pig.casee.entity.liquientity.detail;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class CaseeLiquiDetail {
 
+	/**
+	 * 一审、二审案件最终送达时间
+	 */
+	private LocalDate finalReceiptTime;
 }
