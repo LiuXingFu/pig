d0d9b47bf4da13d96427fe400c5593f58591c68b
yuanduo <1962267412@qq.com>
2022-03-03 15:10:14 +0800

    修改任务节点处理

---
+++ p10-casee/p10-casee-biz/src/main/java/com/pig4cloud/pig/casee/taskscheduling/LiQuiTaskScheduling.java
@@ -0,0 +1,20 @@
+package com.pig4cloud.pig.casee.taskscheduling;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @PACKAGE_NAME: com.pig4cloud.pig.casee.taskscheduling
+ * @ClassNAME: liquiTaskScheduling
+ * @Author: yd
+ * @DATE: 2022/3/3
+ * @TIME: 14:36
+ * @DAY_NAME_SHORT: 周四
+ */
+@Slf4j
+@Component
+public class LiQuiTaskScheduling {
+
+
+
+}
