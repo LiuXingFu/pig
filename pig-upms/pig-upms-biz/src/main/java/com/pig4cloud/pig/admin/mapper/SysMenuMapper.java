93738684041f47d75fcbb4ae6fefcc14651e0548
xiels <Aa123654789>
2022-04-11 17:57:45 +0800

    修改查询权限按钮接口

--- pig-upms/pig-upms-biz/src/main/java/com/pig4cloud/pig/admin/mapper/SysMenuMapper.java
+++ pig-upms/pig-upms-biz/src/main/java/com/pig4cloud/pig/admin/mapper/SysMenuMapper.java
@@ -19,6 +19,7 @@ package com.pig4cloud.pig.admin.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.pig4cloud.pig.admin.api.entity.SysMenu;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
 
@@ -46,5 +47,7 @@ public interface SysMenuMapper extends BaseMapper<SysMenu> {
 	 * @return
 	 */
 	List<String> listPermissionsByRoleIds(String roleIds);
+
+	List<String> selectByRoleId(@Param("roleId") Integer roleId);
 
 }
